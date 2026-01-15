603d4cd12008eee566b579ab891b3f9c91915d6f
Valeriyabogdanova <v.bogdanova2007@ya.ru>
2026-01-15 09:47:00 +0300

    changed

--- 05_lesson/lesson_5_task_3.py
+++ 05_lesson/lesson_5_task_3.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-driver=webdriver.Chrome()
+driver=webdriver.Firefox()
 driver.get("https://the-internet.herokuapp.com/inputs")
 search_place=driver.find_element(By.CSS_SELECTOR, "input[type='number']")
 search_place.send_keys("Sky")
